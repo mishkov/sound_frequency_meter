b66dd69104e8dcb2b379d51e4d9a36b2b1db192b
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-23 12:51:22 +0300

    Remove encoding config

--- lib/sound_frequency_meter_configuration.dart
+++ lib/sound_frequency_meter_configuration.dart
@@ -14,35 +14,16 @@ extension on SampleRate {
   }
 }
 
-enum Encoding { pcm8Bit, pcm16Bit }
-
-extension on Encoding {
-  int toInt() {
-    if (this == Encoding.pcm8Bit) {
-      return 8;
-    } else if (this == Encoding.pcm16Bit) {
-      return 16;
-    } else {
-      throw UnsopportedEncodingRateException(
-        'Given value: $this. Supported values: ${Encoding.pcm8Bit}, ${Encoding.pcm16Bit}',
-      );
-    }
-  }
-}
-
 class SoundFrequencyMeterConfiguration {
   final SampleRate sampleRate;
-  final Encoding encoding;
 
   const SoundFrequencyMeterConfiguration({
     this.sampleRate = SampleRate.k16000,
-    this.encoding = Encoding.pcm8Bit,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'sampleRate': sampleRate.toInt(),
-      'encoding': encoding.toInt(),
     };
   }
 }
@@ -51,10 +32,4 @@ class UnsopportedSampleRateException implements Exception {
   final String message;
 
   UnsopportedSampleRateException(this.message);
-}
-
-class UnsopportedEncodingRateException implements Exception {
-  final String message;
-
-  UnsopportedEncodingRateException(this.message);
 }
