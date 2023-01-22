1318b8bbc25e03d8abfdb798cf74c660ab196a1c
Nikita Mishkov <mishkov.dd@gmail.com>
2023-01-22 16:19:36 +0300

    Implement simple guitar tuner in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,25 +18,30 @@ class TunerApp extends StatefulWidget {
 
 class _TunerAppState extends State<TunerApp> {
   final _soundFrequencyMeter = SoundFrequencyMeter();
+  Stream<double>? _frequencyStream;
 
-  Float64List _data = Float64List(0);
-  final _lastData = List<num>.filled(8, 0.0);
+  List<double> _lastFrequencies = [];
 
   @override
   void initState() {
     super.initState();
-    intFrequencyListener();
+
+    _lastFrequencies = List.generate(
+      100,
+      (index) => 0.0,
+    );
+
+    intFrequencyMeter();
   }
 
-  void intFrequencyListener() {
-    _soundFrequencyMeter.addFrequencyListener((data) {
-      if (data == null) return;
-      if (!mounted) return;
+  @override
+  void dispose() {
+    _soundFrequencyMeter.dispose();
+    super.dispose();
+  }
 
-      setState(() {
-        _data = data;
-      });
-    });
+  void intFrequencyMeter() {
+    _frequencyStream = _soundFrequencyMeter.frequencyStream;
   }
 
   @override
@@ -44,15 +49,44 @@ class _TunerAppState extends State<TunerApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Tuner'),
+          title: const Text('Simple Tuner'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8),
-          child: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: CustomPaint(
-              painter: DataPainter(_data, _lastData),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                StreamBuilder<double>(
+                  stream: _frequencyStream,
+                  builder: (context, snapshot) {
+                    const textStyle =
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+                    if (snapshot.hasData) {
+                      final frequency = snapshot.data!;
+                      _lastFrequencies.removeAt(0);
+                      _lastFrequencies.add(frequency);
+                      return Text(
+                        '${frequency.toStringAsFixed(2)} Hz',
+                        style: textStyle,
+                      );
+                    } else {
+                      return const Text(
+                        '0.0 Hz',
+                        style: textStyle,
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(height: 16),
+                // SizedBox(
+                //   width: double.infinity,
+                //   height: double.infinity,
+                //   child: CustomPaint(
+                //     painter: DataPainter(_lastFrequencies),
+                //   ),
+                // ),
+              ],
             ),
           ),
         ),
@@ -61,57 +95,55 @@ class _TunerAppState extends State<TunerApp> {
   }
 }
 
-class DataPainter extends CustomPainter {
-  List<num> data;
+// class DataPainter extends CustomPainter {
+//   List<double> data;
 
-  List<num> lastData;
+//   DataPainter(this.data);
 
-  DataPainter(this.data, this.lastData);
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     if (data.isEmpty) return;
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    if (data.isEmpty) return;
+//     _drawData(data, size, canvas);
+//   }
 
-    _drawData(data, size, canvas);
-  }
+//   void _drawData(Iterable<num> data, Size size, Canvas canvas) {
+//     final columnWidth = size.width / data.length;
+//     final maxColumnHeight = size.height;
 
-  void _drawData(Iterable<num> data, Size size, Canvas canvas) {
-    final columnWidth = size.width / data.length;
-    final maxColumnHeight = size.height;
+//     // num topValue = data.first;
+//     // num bottomValue = data.first;
+//     // for (int i = 1; i < data.length; i++) {
+//     //   final value = data.elementAt(i);
+//     //   topValue = math.max(value, topValue);
+//     //   bottomValue = math.min(value, bottomValue);
+//     // }
+//     const maxValue = 8; //math.max(topValue.abs(), bottomValue.abs());
 
-    // num topValue = data.first;
-    // num bottomValue = data.first;
-    // for (int i = 1; i < data.length; i++) {
-    //   final value = data.elementAt(i);
-    //   topValue = math.max(value, topValue);
-    //   bottomValue = math.min(value, bottomValue);
-    // }
-    const maxValue = 8; //math.max(topValue.abs(), bottomValue.abs());
+//     for (int i = 0; i < data.length; i++) {
+//       num value;
+//       if (data.elementAt(i) > data.elementAt(i)) {
+//         value = data.elementAt(i);
+//       } else {
+//         value = math.max(data.elementAt(i) - 0.5, 0);
+//       }
 
-    for (int i = 0; i < data.length; i++) {
-      num value;
-      if (data.elementAt(i) > lastData.elementAt(i)) {
-        value = data.elementAt(i);
-      } else {
-        value = math.max(lastData.elementAt(i) - 0.5, 0);
-      }
+//       //value = math.max(value, 1.0);
 
-      //value = math.max(value, 1.0);
+//       data[i] = value.toDouble();
 
-      lastData[i] = value.toDouble();
+//       final columnHeigt = (value / maxValue) * maxColumnHeight;
 
-      final columnHeigt = (value / maxValue) * maxColumnHeight;
+//       final columnPaint = Paint()..color = Colors.blue;
 
-      final columnPaint = Paint()..color = Colors.blue;
+//       final left = i * columnWidth;
+//       final top = maxColumnHeight - columnHeigt;
+//       final column = Rect.fromLTWH(left, top, columnWidth, columnHeigt);
 
-      final left = i * columnWidth;
-      final top = maxColumnHeight - columnHeigt;
-      final column = Rect.fromLTWH(left, top, columnWidth, columnHeigt);
+//       canvas.drawRect(column, columnPaint);
+//     }
+//   }
 
-      canvas.drawRect(column, columnPaint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-}
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+// }
